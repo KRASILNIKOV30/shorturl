4f566fee55436c7bef520645f6a4bfd163db7673
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2025-10-02 11:28:05 +0300

    Создал БД, настроил логирование

--- cmd/shorturl/main.go
+++ cmd/shorturl/main.go
@@ -1,12 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"shorturl/internal/config"
+	"shorturl/internal/lib/logger/sl"
+	"shorturl/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("starting shorturl", slog.String("env", cfg.Env))
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		return
+	}
+
+	_ = storage
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
